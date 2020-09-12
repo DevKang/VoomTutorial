53024589a7239e115b682af055285c8487c88047
DevKang <olddeapo@gmail.com>
2020-09-12 23:09:56 +0900

    주석 추가

--- VoomTutorial/Domain/Interfaces/Repositories/WorkoutRepository.swift
+++ VoomTutorial/Domain/Interfaces/Repositories/WorkoutRepository.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+// Repository는 Aggregate와 1:1로 매칭 시킵니다.
+// https://www.secmem.org/blog/2020/02/19/ddd-aggregate-pattern/
+// https://medium.com/@SlackBeck/애그리게잇-하나에-리파지토리-하나-f97a69662f63
+// Voom의 Aggregate은 Numbers참조
+
 protocol WorkoutRepository {
     func workouts(by bodyPart: BodyPart, completion: ([Workout]) -> Void) throws -> Cancellable?
 }
