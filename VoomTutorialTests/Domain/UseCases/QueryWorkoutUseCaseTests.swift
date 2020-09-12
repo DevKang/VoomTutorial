25a25d037dee4bbfcbf772c378dd0edf477b9c9a
DevKang <olddeapo@gmail.com>
2020-09-12 22:37:32 +0900

    #2-5 테스트 리팩토링
    - 리포지토리에서 받은 Workout들이 모두 정렬이 되는 것을 확인할 수 있게 테스트를 추가한다

--- VoomTutorialTests/Domain/UseCases/QueryWorkoutUseCaseTests.swift
+++ VoomTutorialTests/Domain/UseCases/QueryWorkoutUseCaseTests.swift
@@ -52,13 +52,18 @@ class QueryWorkoutUseCaseTests: XCTestCase {
     func testQueryWorkoutUsecaseShouldNotBeEmptyAndGenerateArrangedWorkoutsByMuscleName() {
         // Given
         let expectation = self.expectation(description: "근육별로 운동이 정렬됩니다")
-        expectation.expectedFulfillmentCount = 1
+        expectation.expectedFulfillmentCount = 2
         let repository = WorkoutRepositoryMock()
         
         // When
-        
-        // 리포지토리에서 받은 Workout의 카운트를 수령한다
         let bodyPart = BodyPart.back
+        // 리포지토리에서 받은 Workout의 카운트를 수령한다
+        var countOnRepository = -1
+        let _ = try? repository.workouts(by: bodyPart) { workouts in
+            countOnRepository = workouts.count
+            expectation.fulfill() // 첫번째 fulfill
+        }
+        
         let requestValue = QueryWorkoutUseCase.RequestValue(bodyPart: bodyPart)
         var arragedWorkouts: [QueryWorkoutUseCase.ArrangedWorkout] = []
         let useCase = QueryWorkoutUseCase(
@@ -90,5 +95,9 @@ class QueryWorkoutUseCaseTests: XCTestCase {
         XCTAssertTrue(!arragedWorkouts.isEmpty)
         // 2. 한 ArragedWorkout의 workouts에는 같은 타겟들만 존재 해야 한다
         XCTAssertTrue(result)
+        // 3. 레포지토리에서 받은 Workout의 갯수와 UseCase를 통해 받은 ArrangedWorkout의 workouts 갯수의 총합이 같아야 한다
+        // -> 누락되는 Workout이 없어야 한다.
+        let totalCount = arragedWorkouts.reduce(0, { $0 + $1.workouts.count })
+        XCTAssertEqual(totalCount, countOnRepository)
     }
 }
